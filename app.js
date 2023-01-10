ea383b5fb51a7516c8189e80a9fc77fb85315cb3
Mariana Navarro <maronare10@gmail.com>
2023-01-10 02:22:40 -0500

    list0ando pokemons, trayendo el id e image y aplicando axios

--- app.js
+++ app.js
@@ -1,36 +1,59 @@
-const fetchPokemons = async () => {
-  // https://pokeapi.co/api/v2/pokemon
+let page = 1
 
-  const url = 'https://pokeapi.co/api/v2/pokemon'
-  const response = await fetch(url)
-  const data = await response.json()
-  // console.log(response)
-  // console.log(data)
-  const dataResults = data.results
-  // console.log(dataResults)
+const fetchPokemons = async (page = 1) => {
+  const limit = 9
+  const offset = (page - 1) * limit
+  const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+  const response = await axios.get(url) // Devuelve una promesa
+  const dataResults = response.data.results.map(pokemon => {
+    const id = pokemon.url.split('/').at(6)
+    const image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
+    return {
+      ...pokemon,
+      id,
+      image
+    }
+  })
+
   return dataResults
 }
 
 const renderPokemons = (pokemons) => {
-  const pokemonList = document.getElementById('pokemonList')
+  const pokemonsList = document.getElementById('pokemonsList')
 
   let elements = ''
 
   pokemons.forEach(pokemon => {
-    elements += `<h2>${pokemon.name}</h2>`
+    elements += `<article class='pokemons-item'>
+      <img src='${pokemon.image}' width='80' height='80'></img>
+      <h2>#${pokemon.id} ${pokemon.name}</h2>
+    </article>
+    `
   })
-
-  pokemonList.innerHTML = elements
-
+  pokemonsList.innerHTML = elements
 }
-
-
 
 const documentReady = async () => {
-  const pokemons = await fetchPokemons()
-  renderPokemons(pokemons)
+  const nextPage = document.getElementById('nextPage')
+  const prevtPage = document.getElementById('prevPage')
+  const currentPage = document.getElementById('currentPage')
 
+  nextPage.addEventListener('click', async () => {
+    const pokemons = await fetchPokemons(++page)
+    renderPokemons(pokemons)
+    currentPage.innerHTML = page
+  })
+
+  prevPage.addEventListener('click', async () => {
+    const pokemons = await fetchPokemons(--page)
+    renderPokemons(pokemons)
+    currentPage.innerHTML = page
+  })
+
+
+  const pokemons = await fetchPokemons()
+  // console.log(pokemons)
+  renderPokemons(pokemons)
 }
 
-document.addEventListener('DOMContentLoaded', documentReady);
-
+document.addEventListener('DOMContentLoaded', documentReady); 
